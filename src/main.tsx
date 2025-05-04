3c4325feef95766f089cc3ca87d0ed0d6662d49d
WatkornFeng <watcharakornk2022@gmail.com>
2025-05-04 16:46:47 +0700

    add asset images

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+
 import store from "./store";
 import { theme } from "./theme";
 
